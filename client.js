b8075239f3ee4a64126473d30ef73534fc7e6561
ManuelMaciel <manuelmaciel7001@hotmail.com>
2021-05-27 20:05:03 -0400

    client can close the connection and server display the end of connection

--- client.js
+++ client.js
@@ -4,14 +4,22 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 })
 
+const END = 'END';
+
 const socket = new Socket();
 
 socket.connect({ host: "localhost", port: "8000" });
 socket.setEncoding("utf-8");
 
-readline.on('line', (line) => {
-  socket.write(line)
+readline.on('line', (message) => {
+  socket.write(message);
+  if(message === END){
+    socket.end();
+  }
 })
+
 socket.on("data", (data) => {
   console.log(data);
 });
+
+socket.on('close', () => process.exit(0))
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { Server } = require("net");
 const server = new Server();
 
 server.on("connection", (socket) => {
+  const END = 'END';
   const remoteSocket = ` ${socket.remoteAddress}:${socket.remotePort}`;
   console.log(
     `New connection from ${remoteSocket}`
@@ -11,8 +12,13 @@ server.on("connection", (socket) => {
   socket.setEncoding('utf-8');
   socket.on("data", (message) => {
     // socket.write(data);
-    console.log(`${remoteSocket} -> ${message}`)
+    if(message === END) {
+      socket.end();
+    } else {
+      console.log(`${remoteSocket} -> ${message}`)
+    }
   });
+  socket.on('close', () => console.log(`Connection with ${remoteSocket} closed`))
 });
 
 server.listen({ port: 8000, host: "0.0.0.0" }, () => {
